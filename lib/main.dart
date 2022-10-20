7b8c91196de0e0cfbf8a800db9f05cfd16670e9d
lenovo <eman>
2022-10-20 20:32:38 +0200

    add splash screen..

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 return MaterialApp(
-  title: 'my app',
-  routes: {
-homescreen.routename:(context)=>homescreen(),
-  },
-  initialRoute:homescreen.routename ,
-);
+  title: 'My App',
+      routes: {
+        homescreen.routename: (context) => homescreen(),
+      },
+      initialRoute: homescreen.routename,
+    );
   }
 }
